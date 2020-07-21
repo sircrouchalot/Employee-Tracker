82ffe0ce3c1fa59ec41aaa63980fc6072c8bace9
Robert Alex Crouch <acrouch06@gmail.com>
2020-07-20 20:22:19 -0500

    created employee class

---
+++ classes/Employee.js
@@ -0,0 +1,28 @@
+class Employee {
+    constructor(firstName, lastName, role, salary, manager) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.role = role;
+        this.salary = salary;
+        this.manager = manager;
+        this.role = "";
+    }
+
+    getName() {
+        return this.firstName + this.lastName;
+    }
+
+    getRole() {
+        return this.role;
+    }
+    
+    getSalary() {
+        return this.salary;
+    }
+
+    getManager() {
+        return this.manager;
+    }
+}
+
+module.exports = Employee;
