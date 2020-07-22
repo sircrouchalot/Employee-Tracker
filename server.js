cc8ec0293e971b286665a6e433558d07b5de8cd8
Robert Alex Crouch <acrouch06@gmail.com>
2020-07-21 21:28:02 -0500

    added ability to add employees

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
-const { connect } = require("http2");
+const Employee = require("./classes/Employee.js");
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -12,68 +12,177 @@ var connection = mysql.createConnection({
 
 var departments = ["Sales", "Engineering", "Legal", "Finance"];
 
+var roles = [
+  "Engineer",
+  "Lead Engineer",
+  "Salesperson",
+  "Sales Lead",
+  "Paralegal",
+  "Lawyer",
+  "Accountant",
+];
+
+var managers = [];
+
+var newEmployeePrompts = [
+  {
+    type: "input",
+    message: "What is their first name?",
+    name: "firstName"
+  },
+  {
+    type: "input",
+    message: "What is their last name?",
+    name: "lastName"
+  },
+  {
+    type: "list",
+    message: "What is their role?",
+    choices: roles,
+    name: "title"
+  },
+  {
+    type: "input",
+    message: "Enter their manager's id:",
+    name: "manager"
+  }
+]
+
 var prompts = [
   {
-    type: "rawlist",
+    type: "list",
     message: "What would you like to do?",
     choices: [
       "View All Employees",
       "View All Employees by Department",
       "View All Employees by Role",
-      "View All Employees by Manager",
       "Add Employee",
       "Remove Employee",
       "Update Employee Role",
+      "EXIT",
     ],
     name: "action",
   },
   {
-    type: "rawlist",
+    type: "list",
     message: "Which department?",
     choices: departments,
     name: "department",
     when: (response) => response.action === "View All Employees by Department",
   },
+  {
+    type: "list",
+    message: "Which role?",
+    choices: roles,
+    name: "role",
+    when: (response) => response.action === "View All Employees by Role",
+  },
+  {
+    type: "list",
+    message: "Which manager?",
+    choices: roles,
+    name: "manager",
+    when: (response) => response.action === "View All Employees by Manager",
+  },
 ];
 
 connection.connect(function (err) {
   if (err) throw err;
-  runInquirer();
+  start();
 });
 
-function runInquirer() {
+function start() {
   inquirer.prompt(prompts).then(function (response) {
     switch (response.action) {
       case "View All Employees":
-        returnEmployeeTable();
-        runInquirer();
-        return;
-      case "View All Employees by Department":
-        employeesByDept(response.department);
-        runInquirer();
-        return;
-      default:
+        returnAll();
         break;
+      case "View All Employees by Department":
+        viewDepts(response.department);
+        break;
+      case "View All Employees by Role":
+        viewRoles(response.role);
+        break;
+      case "Add Employee":
+        addEmployee(response);
+        break;
+      case "Remove Employee":
+        break;
+      case "Update Employee Role":
+        break;
+      default:
+        process.exit();
     }
   });
-}
 
-function returnEmployeeTable() {
-  connection.query(`SELECT * FROM employees`, function (err, res) {
-    if (err) throw err;
-    console.log("\n");
-    console.table(res);
-  });
-}
+  function returnAll() {
+    connection.query(
+      `SELECT employees.id, employees.first_name AS First, employees.last_name AS Last, departments.name AS Department, role.title AS Title, role.salary AS Salary, employees.manager_id
+      FROM employees
+      LEFT JOIN role ON role.id = employees.role_id
+      LEFT JOIN departments ON role.department_id = departments.id;`,
+      function (err, res) {
+        if (err) throw err;
+        console.log("\n");
+        console.table(res);
+        start();
+      }
+    );
+  }
 
-function employeesByDept(dept) {
-  connection.query(
-    `SELECT * FROM employees WHERE Department = ?`,
-    [dept],
-    (err, results) => {
-      if (err) throw err;
-      console.log("Finding employees by department: " + dept);
-      console.table(results);
-    }
-  );
+  function viewDepts(dept) {
+    connection.query(
+      `SELECT employees.id, employees.first_name AS First, employees.last_name AS Last, departments.name AS Department, role.title AS Title, role.salary AS Salary, employees.manager_id
+      FROM employees
+      LEFT JOIN role ON role.id = employees.role_id
+      LEFT JOIN departments ON role.department_id = departments.id
+      WHERE Department = ?`,
+      [dept],
+      (err, results) => {
+        if (err) throw err;
+        console.log("Finding employees by department: " + dept);
+        console.table(results);
+        start();
+      }
+    );
+  }
+
+  function viewRoles(role) {
+    connection.query(
+      `SELECT employees.id, employees.first_name AS First, employees.last_name AS Last, departments.name AS Department, role.title AS Title, role.salary AS Salary, employees.manager_id
+      FROM employees
+      LEFT JOIN role ON role.id = employees.role_id
+      LEFT JOIN departments ON role.department_id = departments.id
+      WHERE Title = ?`,
+      [role],
+      (err, res) => {
+        if (err) throw err;
+        console.log("Finding employees by role: " + role);
+        console.table(res);
+        start();
+      }
+    );
+  }
+
+  function addEmployee() {
+    inquirer
+    .prompt(newEmployeePrompts)
+    .then(function(response) {
+      let newEmployee = new Employee(response.firstName, response.lastName, response.title, response.manager);
+      let role_id = roles.findIndex((element) => element === response.title) + 1;
+      if (response.manager === '') {
+        response.manager = null;
+      }
+      connection.query(
+        `INSERT INTO employees(first_name, last_name, role_id, manager_id)
+        VALUES (?, ?, ?, ?);`,
+        [response.firstName, response.lastName, role_id, response.manager],
+        (err, res) => {
+          if (err) throw err;
+          console.table("Adding employee...SUCCESSFUL!")
+          start();
+        }
+      );
+    });
+  }
 }
