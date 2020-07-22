4cba58c3248811a990fef2034073d23e40bcd067
Robert Alex Crouch <acrouch06@gmail.com>
2020-07-22 00:10:08 -0500

    edits

--- data/employee_schema.sql
+++ data/employee_schema.sql
@@ -1,17 +1,25 @@
-DROP DATABASE IF EXISTS employee_db;
+-- DROP DATABASE IF EXISTS employee_db;
 
-CREATE DATABASE employee_db;
+-- CREATE DATABASE employee_db;
 
 USE employee_db;
 
-CREATE TABLE employees (
-    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL,
-    role VARCHAR(50) NOT NULL,
-    department VARCHAR(50) NOT NULL,
-    salary INT NOT NULL,
-    manager VARCHAR(50)
-);
+-- CREATE TABLE employees (
+--     id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+--     first_name VARCHAR(50) NOT NULL,
+--     last_name VARCHAR(50) NOT NULL,
+--     role_id INT NOT NULL,
+--     manager_id INT
+-- );
 
-SELECT * FROM employees;
+-- CREATE TABLE departments (
+-- 	id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+-- 	name VARCHAR(30) NOT NULL
+-- );
+
+-- CREATE TABLE role (
+-- 	id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+-- 	title VARCHAR(30) NOT NULL,
+--     salary DECIMAL NOT NULL,
+--     department_id INT NOT NULL
+-- );
